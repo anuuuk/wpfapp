660e5208ef78a97cae48fd5ea6192b9a8d156cf3
Автамонова Анна Александровна <Avtamonova.A.A@corp.kp11.ru>
2024-02-07 14:02:56 +0300

    first commit

---
+++ Avtamonova/AdminWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Avtamonova
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class AdminWindow : Window
+    {
+        public AdminWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ManageWindow manageWindow = new ManageWindow();
+            manageWindow.Show();
+            this.Close();
+        }
+    }
+}
